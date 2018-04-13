95b8eeb9495cb1420806e3371cd23fed7c830177
Matthew <matthewmichaelmartinez@gmail.com>
2018-04-13 15:55:18 -0700

    final push to heroku

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,11 +1,6 @@
 # This file should contain all the record creation needed to seed the database with its default values.
 # The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
 #
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
-
 
 User.create(username: 'Matthew M.', email: 'MattWanderers@gmail.com', password: 'javajava')
 User.create(username: 'Demo Account', email: 'demo@demo.com', password: 'password')
@@ -29,7 +24,7 @@ Event.create(
   description: "Chef Ian Kittichai is one of Thailand's most successful Iron Chefs.
   He has created award-winning restaurants across the globe including Bangkok, Barcelona, Hong Kong, Mumbai, New York and Singapore.
   One of his most successful outlets, Issaya Siamese Club, has been listed as one of Asia’s 50 Best Restaurants, sponsored by S. Pellegrino and Aqua Panna since 2014 to today.",
-  organizer_id: 3,
+  organizer_id: 7,
   start: DateTime.now,
   end: (DateTime.now.advance(:hours => 3)),
   img_url: "http://res.cloudinary.com/do2qcwqsc/image/upload/v1523568969/event_seed_1.png",
@@ -37,222 +32,158 @@ Event.create(
   category_id: 1
 )
 
-# Music: 0,
-# :"Food & Drink" => 1,
-# Classes: 2,
-# Arts: 3,
-# Parties: 4,
-# :"Sports & Wellness" => 5,
-# Networking: 6
-
-Event.create(
-  title: "These Guys Sing",
-  description: "They sing AND dance.",
-  organizer_id: (1..9).to_a.sample,
-  start: (x = Faker::Time.forward(40)),
-  end: (x + 2.hours),
-  img_url: "https://res.cloudinary.com/do2qcwqsc/image/upload/v1523467605/tg5ksglitcbarps5crvi.jpg",
-  location_id: (1..6).to_a.sample,
-  category_id: 0
-)
-
-# Music: 0,
-# :"Food & Drink" => 1,
-# Classes: 2,
-# Arts: 3,
-# Parties: 4,
-# :"Sports & Wellness" => 5,
-# Networking: 6
-
-Event.create(
-  title: "These Flowers Sing",
-  description: "They sing very quietly.",
-  organizer_id: (1..9).to_a.sample,
-  start: (x = Faker::Time.forward(40)),
-  end: (x + 2.hours),
-  img_url: "https://res.cloudinary.com/do2qcwqsc/image/upload/v1523462719/sample.jpg",
-  location_id: (1..6).to_a.sample,
-  category_id: 4
-)
-
-# Music: 0,
-# :"Food & Drink" => 1,
-# Classes: 2,
-# Arts: 3,
-# Parties: 4,
-# :"Sports & Wellness" => 5,
-# Networking: 6
-
 Event.create(
   title: "AppAcademy Hackathon",
   description: "Come have fun and hack something with us. We have prizes!",
   organizer_id: 4,
-  start: (x = Faker::Time.forward(40)),
-  end: (x + 2.hours),
-  img_url: "https://res.cloudinary.com/do2qcwqsc/image/upload/v1523565827/kd6auoy8bzdkwrrcihlp.jpg",
-  location_id: 1,
-  category_id: 6
+  start: (x = Faker::Time.forward(90)),
+end: (x + 2.hours),
+img_url: "https://res.cloudinary.com/do2qcwqsc/image/upload/v1523565827/kd6auoy8bzdkwrrcihlp.jpg",
+location_id: 1,
+category_id: 6
 )
-# Music: 0,
-# :"Food & Drink" => 1,
-# Classes: 2,
-# Arts: 3,
-# Parties: 4,
-# :"Sports & Wellness" => 5,
-# Networking: 6
-
-Event.create(
-  title: "San Jose State Quidditch TRY-OUTS",
-  description: "Come prove your mettle! Our team has gone to nationals and we regularly play other local teams (and Kick their butts)!",
-  organizer_id: 1,
-  start: (x = Faker::Time.forward(40)),
-  end: (x + 2.hours),
-  img_url: "https://res.cloudinary.com/do2qcwqsc/image/upload/v1523589793/sjsu-quidditch.jpg",
-  location_id: 4,
-  category_id: 5
-)
-# Music: 0,
-# :"Food & Drink" => 1,
-# Classes: 2,
-# Arts: 3,
-# Parties: 4,
-# :"Sports & Wellness" => 5,
-# Networking: 6
-
-Event.create(
-  title: "Typical day at Cal. Come protest with us!",
-  description: "Bring bottled water and your outside voice.",
-  organizer_id: 2,
-  start: (x = Faker::Time.forward(40)),
-  end: (x + 2.hours),
-  img_url: "https://res.cloudinary.com/do2qcwqsc/image/upload/v1523568510/AUG_25-copy-e1503276168691.jpg",
-  location_id: (1..6).to_a.sample,
-  category_id: 6
-)
-# Music: 0,
-# :"Food & Drink" => 1,
-# Classes: 2,
-# Arts: 3,
-# Parties: 4,
-# :"Sports & Wellness" => 5,
-# Networking: 6
-Event.create(
-  title: "First Friday San Jose(Now coming to a city near YOU!)",
-  description: "Now expanding to random days of the week and random cities throughout the bay area!
-  See if you can find our pop-up event full of street-food, live music, and more!",
-  organizer_id: (1..9).to_a.sample,
-  start: (x = Faker::Time.forward(40)),
-  end: (x + 2.hours),
-  img_url: "https://res.cloudinary.com/do2qcwqsc/image/upload/v1523566971/1_ky40j5X-OrINlY3muivD4A.jpg",
-  location_id: (1..6).to_a.sample,
-  category_id: 4
-)
-
-Event.create(
-  title: "A really good cat",
-  description: "The title says it all.",
-  organizer_id: 3,
-  start: (x = Faker::Time.forward(40)),
-  end: (x + 2.hours),
-  img_url: "https://res.cloudinary.com/do2qcwqsc/image/upload/v1523484508/weadae04w1opvh7gi3aa.jpg",
-  location_id: (1..6).to_a.sample,
-  category_id: 3
-)
-# Music: 0,
-# :"Food & Drink" => 1,
-# Classes: 2,
-# Arts: 3,
-# Parties: 4,
-# :"Sports & Wellness" => 5,
-# Networking: 6
 
 Event.create(
   title: "San Francisco Community Choir performs this year's top Pop hits!",
   description: "They'll be performing a sampliung of this year's top songs plus a smattering of Michael Jackson.
   Get your tickets now, you don't want to miss out!",
   organizer_id: (1..9).to_a.sample,
-  start: (x = Faker::Time.forward(40)),
+  start: (x = Faker::Time.forward(90)),
+end: (x + 2.hours),
+img_url: "https://res.cloudinary.com/do2qcwqsc/image/upload/v1523567174/maxresdefault.jpg",
+location_id: 1,
+category_id: 0
+)
+
+Event.create(
+  title: "These Guys Sing",
+  description: "They sing AND dance.",
+  organizer_id: (1..9).to_a.sample,
+  start: (x = Faker::Time.forward(90)),
   end: (x + 2.hours),
-  img_url: "https://res.cloudinary.com/do2qcwqsc/image/upload/v1523567174/maxresdefault.jpg",
+  img_url: "https://res.cloudinary.com/do2qcwqsc/image/upload/v1523467605/tg5ksglitcbarps5crvi.jpg",
   location_id: (1..6).to_a.sample,
   category_id: 0
 )
 
-# Music: 0,
-# :"Food & Drink" => 1,
-# Classes: 2,
-# Arts: 3,
-# Parties: 4,
-# :"Sports & Wellness" => 5,
-# Networking: 6
+Event.create(
+  title: "San Jose State Quidditch TRY-OUTS",
+  description: "Come prove your mettle! Our team has gone to nationals and we regularly play other local teams (and Kick their butts)!",
+  organizer_id: 1,
+  start: (x = Faker::Time.forward(90)),
+end: (x + 2.hours),
+img_url: "https://res.cloudinary.com/do2qcwqsc/image/upload/v1523589793/sjsu-quidditch.jpg",
+location_id: 4,
+category_id: 5
+)
+
+Event.create(
+  title: "Beautiful Music. Every time.",
+  description: "'Nuff said.",
+  organizer_id: 1,
+  start: (x = Faker::Time.forward(90)),
+  end: (x + 2.hours),
+  img_url: "https://res.cloudinary.com/do2qcwqsc/image/upload/v1523567226/CyrilleDayme_wide-4f552a8c11a43b36c0cca621d7057bc4db69e909-s1200.png",
+  location_id: (1..6).to_a.sample,
+  category_id: 0
+)
+
+Event.create(
+  title: "These Flowers Sing",
+  description: "They sing very quietly.",
+  organizer_id: (1..9).to_a.sample,
+  start: (x = Faker::Time.forward(90)),
+  end: (x + 2.hours),
+  img_url: "https://res.cloudinary.com/do2qcwqsc/image/upload/v1523462719/sample.jpg",
+  location_id: (1..6).to_a.sample,
+  category_id: 4
+)
 
 Event.create(
   title: "First Friday San Jose",
-  description: "Now expanding to random days of the week and random cities throughout the bay area!
+  description: "Now expanding to random days of the week!
   See if you can find our pop-up event full of street-food, live music, and more!",
   organizer_id: (1..9).to_a.sample,
-  start: (x = Faker::Time.forward(40)),
+  start: (x = Faker::Time.forward(90)),
+end: (x + 2.hours),
+img_url: "https://res.cloudinary.com/do2qcwqsc/image/upload/v1523566971/1_ky40j5X-OrINlY3muivD4A.jpg",
+location_id: 4,
+category_id: 1
+)
+
+Event.create(
+  title: "Soft Roller Derby",
+  description: "Well, really we're just skating and laughing and having a good time all around. Bring your pads and your game face. Just in case.",
+  organizer_id: 1,
+  start: (x = Faker::Time.forward(90)),
+end: (x + 2.hours),
+img_url: "https://res.cloudinary.com/do2qcwqsc/image/upload/v1523655044/648c69342bf8e92429ae800c829eed11--roller-skating-roller-derby.jpg",
+location_id: (1..6).to_a.sample,
+category_id: 5
+)
+
+Event.create(
+  title: "Typical day at Cal. Come protest with us!",
+  description: "Bring bottled water and your outside voice.",
+  organizer_id: 2,
+  start: (x = Faker::Time.forward(90)),
   end: (x + 2.hours),
-  img_url: "https://res.cloudinary.com/do2qcwqsc/image/upload/v1523566971/1_ky40j5X-OrINlY3muivD4A.jpg",
+  img_url: "https://res.cloudinary.com/do2qcwqsc/image/upload/v1523568510/AUG_25-copy-e1503276168691.jpg",
+  location_id: (1..6).to_a.sample,
+  category_id: 6
+)
+
+Event.create(
+  title: "A really good cat",
+  description: "The title says it all.",
+  organizer_id: 3,
+  start: (x = Faker::Time.forward(90)),
+  end: (x + 2.hours),
+  img_url: "https://res.cloudinary.com/do2qcwqsc/image/upload/v1523484508/weadae04w1opvh7gi3aa.jpg",
   location_id: (1..6).to_a.sample,
   category_id: 3
 )
-# Music: 0,
-# :"Food & Drink" => 1,
-# Classes: 2,
-# Arts: 3,
-# Parties: 4,
-# :"Sports & Wellness" => 5,
-# Networking: 6
+
+Event.create(
+  title: "Coastal Clean Up",
+  description: "Our natural watersheds are dirty and tarnished. Do your part and lets see how many tons of trash we can eliminate! Free tacos.",
+  organizer_id: 1,
+  start: (x = Faker::Time.forward(90)),
+  end: (x + 2.hours),
+  img_url: "https://res.cloudinary.com/do2qcwqsc/image/upload/v1523655030/MLK-Shoreline_CCD2015.jpg",
+  location_id: (1..6).to_a.sample,
+  category_id: 6
+)
 
 Event.create(
   title: "Alameda Art Walk",
   description: "The title says it all.",
   organizer_id: (1..9).to_a.sample,
-  start: (x = Faker::Time.forward(40)),
+  start: (x = Faker::Time.forward(90)),
   end: (x + 2.hours),
   img_url: "https://res.cloudinary.com/do2qcwqsc/image/upload/v1523566842/whiteelephantsale_mural_by_Griffin_Vogue_doty.jpg",
   location_id: 6,
   category_id: 3
 )
+
+
 Event.create(
-  title: "First Friday San Jose",
-  description: "Now expanding to random days of the week and random cities throughout the bay area!
-  See if you can find our pop-up event full of street-food, live music, and more!",
-  organizer_id: (1..9).to_a.sample,
-  start: (x = Faker::Time.forward(40)),
-  end: (x + 2.hours),
-  img_url: "https://res.cloudinary.com/do2qcwqsc/image/upload/v1523566971/1_ky40j5X-OrINlY3muivD4A.jpg",
-  location_id: (1..6).to_a.sample,
-  category_id: 1
-)
-Event.create(
-  title: "Let's learn to Fish",
+  title: "Learn to Fish",
   description: "Come on down and learn to fish. Hosted by the 'We just like to fish' local chapter.",
   organizer_id: (1..9).to_a.sample,
-  start: (x = Faker::Time.forward(40)),
+  start: (x = Faker::Time.forward(90)),
   end: (x + 2.hours),
   img_url: "https://res.cloudinary.com/do2qcwqsc/image/upload/v1523566887/IMG_8039.jpg",
   location_id: (1..6).to_a.sample,
   category_id: 5
 )
-Event.create(
-  title: "First Friday San Jose(Now coming to a city near YOU!)",
-  description: "Now expanding to random days of the week and random cities throughout the bay area!
-  See if you can find our pop-up event full of street-food, live music, and more!",
-  organizer_id: (1..9).to_a.sample,
-  start: (x = Faker::Time.forward(40)),
-  end: (x + 2.hours),
-  img_url: "https://res.cloudinary.com/do2qcwqsc/image/upload/v1523566971/1_ky40j5X-OrINlY3muivD4A.jpg",
-  location_id: (1..6).to_a.sample,
-  category_id: 4
-)
+
 Event.create(
   title: "Learn to chop like a pro",
   description: "Master-Chef Alton Ramsey teaches you only the best way to chop your vegetables.
   As a bonus, learn to slice bread, salmon, and tender tofu. ",
   organizer_id: (1..9).to_a.sample,
-  start: (x = Faker::Time.forward(40)),
+  start: (x = Faker::Time.forward(90)),
   end: (x + 2.hours),
   img_url: "https://res.cloudinary.com/do2qcwqsc/image/upload/v1523567071/Bagel_Spread.jpg",
   location_id: (1..6).to_a.sample,
@@ -264,9 +195,122 @@ Event.create(
   description: "Come on down and let loose! We're here to mingle and relax after a long week of the daily grind.
   Meet some like-minded folxs and grab a beer or two",
   organizer_id: 1,
-  start: (x = Faker::Time.forward(40)),
+  start: (x = Faker::Time.forward(90)),
   end: (x + 2.hours),
   img_url: "https://res.cloudinary.com/do2qcwqsc/image/upload/v1523568654/1281489019cocktail.jpg",
   location_id: (1..6).to_a.sample,
   category_id: 6
+)
+
+Event.create(
+  title: "California Dodgeball Free-for-All",
+  description: "Don't bring any wrenchs. Please. Just your fun-loving selves! No teams, paid refs, come on out!",
+  organizer_id: 1,
+  start: (x = Faker::Time.forward(90)),
+  end: (x + 2.hours),
+  img_url: "https://res.cloudinary.com/do2qcwqsc/image/upload/v1523650424/ngla7lmfqw4wkhqo93aq.jpg",
+  location_id: 2,
+  category_id: 5
+)
+
+Event.create(
+  title: "Best burger Contest",
+  description: "Can you make a better burger? the best? Then test your skills against other like-minded folk.
+  We get together every couple of months and cook our favoriate food.",
+  organizer_id: (1..9).to_a.sample,
+  start: (x = Faker::Time.forward(90)),
+end: (x + 2.hours),
+img_url: "https://res.cloudinary.com/do2qcwqsc/image/upload/v1523655001/image.jpg",
+location_id: (1..6).to_a.sample,
+category_id: 1
+)
+
+Event.create(
+  title: "Coastal Clean Up",
+  description: "Our natural watersheds are dirty and tarnished. Do your part and lets see how many tons of trash we can eliminate! Free tacos.",
+  organizer_id: 1,
+  start: (x = Faker::Time.forward(90)),
+  end: (x + 2.hours),
+  img_url: "https://res.cloudinary.com/do2qcwqsc/image/upload/v1523655030/MLK-Shoreline_CCD2015.jpg",
+  location_id: (1..6).to_a.sample,
+  category_id: 6
+)
+
+Event.create(
+  title: "Audience Participates Jazz Night!",
+  description: "Join our house band and go to town for a song or two or more! All skill levels welcome.",
+  organizer_id: 1,
+  start: (x = Faker::Time.forward(90)),
+  end: (x + 2.hours),
+  img_url: "https://res.cloudinary.com/do2qcwqsc/image/upload/v1523655009/built-to-spill-20-l.jpg",
+  location_id: (1..6).to_a.sample,
+  category_id: 0
+)
+
+
+Event.create(
+  title: "BIKE PAARTYY",
+  description: "We take regular rides with our 200 closest friends. Bring your lights and reflective clothing and come prepared to BIIIKKEE PARTTYYYY",
+  organizer_id: 1,
+  start: (x = Faker::Time.forward(90)),
+  end: (x + 2.hours),
+  img_url: "https://res.cloudinary.com/do2qcwqsc/image/upload/v1523655037/NEWS_bike-party.jpg",
+  location_id: (1..6).to_a.sample,
+  category_id: 4
+)
+
+Event.create(
+  title: "Fox Theater Mystery Band",
+  description: "We won't tell you who they are, but we will guarantee the price of your ticket if you leave unsatisfied!
+  We're that confident that you'll have a good time.",
+  organizer_id: 1,
+  start: (x = Faker::Time.forward(90)),
+  end: (x + 2.hours),
+  img_url: "https://res.cloudinary.com/do2qcwqsc/image/upload/v1523654994/floggingmolly-8660.jpg",
+  location_id: (1..6).to_a.sample,
+  category_id: 0
+)
+
+Event.create(
+  title: "Mario Kart Tournament.",
+  description: "'Nuff said.",
+  organizer_id: 1,
+  start: (x = Faker::Time.forward(90)),
+  end: (x + 2.hours),
+  img_url: "https://res.cloudinary.com/do2qcwqsc/image/upload/v1523655015/-visit_to_AFK_Gamer_Lounge-20000000009049832-500x375.jpg",
+  location_id: (1..6).to_a.sample,
+  category_id: 5
+)
+
+Event.create(
+  title: "Beautiful Music. Every time.",
+  description: "'Nuff said.",
+  organizer_id: 1,
+  start: (x = Faker::Time.forward(90)),
+  end: (x + 2.hours),
+  img_url: "https://res.cloudinary.com/do2qcwqsc/image/upload/v1523567226/CyrilleDayme_wide-4f552a8c11a43b36c0cca621d7057bc4db69e909-s1200.png",
+  location_id: (1..6).to_a.sample,
+  category_id: 0
+)
+
+Event.create(
+  title: "These Guys Don't Sing",
+  description: "They only pose.",
+  organizer_id: (1..9).to_a.sample,
+  start: (x = Faker::Time.forward(90)),
+  end: (x + 2.hours),
+  img_url: "https://res.cloudinary.com/do2qcwqsc/image/upload/v1523467605/tg5ksglitcbarps5crvi.jpg",
+  location_id: (1..6).to_a.sample,
+  category_id: 0
+)
+
+Event.create(
+  title: "Secret Speakeasy-1 Night Only.",
+  description: "They only pose.",
+  organizer_id: (1..9).to_a.sample,
+  start: (x = Faker::Time.forward(90)),
+  end: (x + 2.hours),
+  img_url: "https://res.cloudinary.com/do2qcwqsc/image/upload/v1523655001/image.jpg",
+  location_id: (1..6).to_a.sample,
+  category_id: 1
 )
