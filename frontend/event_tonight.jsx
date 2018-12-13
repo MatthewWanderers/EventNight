8c834ed30efa65f991d9babe0e27bb9d93d3eb0b
Matthew <matthewmichaelmartinez@gmail.com>
2018-12-13 15:24:59 -0800

    working on defaulting to logged in demo account

--- frontend/event_tonight.jsx
+++ frontend/event_tonight.jsx
@@ -2,9 +2,20 @@ import React from 'react';
 import ReactDOM from 'react-dom'
 import Root from './components/root';
 import configureStore from './store/store';
+import * as APIUtil from './util/session_api_util';
 
 document.addEventListener('DOMContentLoaded',() => {
   let store;
+  if (window.demoUser) {
+    console.log("made it through check");
+    const user = {
+      username: 'Demo Account',
+      password: 'password'
+    };
+    APIUtil.login(user);
+    delete window.demoUser;
+  }
+  console.log("past first if");
   if (window.currentUser) {
     const preloadedState = { session: { currentUser: window.currentUser } };
     store = configureStore(preloadedState);
